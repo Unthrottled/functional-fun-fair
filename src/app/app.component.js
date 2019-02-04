03beb5b8ee9e96389e80fc337b38c867db60696d
DATBOI <alexsimons9999@gmail.com>
2019-02-03 18:45:02 -0600

    More scrolling to the top when the route changes, yo

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -5,18 +5,32 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 require("./app.component.htm");
+var router_1 = require("@angular/router");
 var AppComponent = /** @class */ (function () {
-    function AppComponent() {
+    function AppComponent(router) {
+        this.router = router;
         this.versionNumber = "v.1.0.2";
     }
+    AppComponent.prototype.ngOnInit = function () {
+        this.router.events.subscribe(function (evt) {
+            if (!(evt instanceof router_1.NavigationEnd)) {
+                return;
+            }
+            window.scrollTo(0, 0);
+        });
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'angular-application',
             template: require('./app.component.htm')
-        })
+        }),
+        __metadata("design:paramtypes", [router_1.Router])
     ], AppComponent);
     return AppComponent;
 }());
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from '@angular/core';
 import "./app.component.htm";
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
     selector: 'angular-application',
     template: require('./app.component.htm')
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
     versionNumber: string = "v.1.0.2";
+
+    constructor(private router: Router){
+
+
+    }
+
+    ngOnInit(): void {
+        this.router.events.subscribe((evt) => {
+            if (!(evt instanceof NavigationEnd)) {
+                return;
+            }
+            window.scrollTo(0, 0);
+        });
+    }
 
 }
