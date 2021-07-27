ecd7838ea7d5405ee31c3bb6bc73c28d596c8015
DATBOI <alexsimons9999@gmail.com>
2021-07-27 17:26:20 -0500

    I guess I did stuff

--- src/app/background.component.js
+++ src/app/background.component.js
@@ -48,7 +48,7 @@ var BackgroundComponent = /** @class */ (function () {
         ctx.clearRect(0, 0, w, h);
         ctx.beginPath();
         ctx.moveTo(0, h * 0.85);
-        ctx.quadraticCurveTo(w / 1.85, h, w, 0);
+        ctx.quadraticCurveTo(w / 1.85, h, w, h / 2);
         ctx.lineTo(w, h);
         ctx.lineTo(0, h);
         ctx.fillStyle = '#EEEEEE';
